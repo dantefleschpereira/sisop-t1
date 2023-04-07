9a2a0e751e24dacd348a1ac2857726e3b892ef77
Dante <48799984+dantefleschpereira@users.noreply.github.com>
2023-04-07 14:02:38 -0300

    feat: alteração id para pid no processo.py

--- processo.py
+++ processo.py
@@ -2,24 +2,24 @@
 class Processo:
 
     # Essa linha possibilita gerar ids sequenciais automaticamente para cada processo criado
-    proximo_id = 1
+    proximo_pid = 1
 
     # Construtor com os atributos de um processo
     def __init__(self, id=None, logica=None, tempo_chegada=None, prioridade=None, quantum=None, tempo_execucao=None):
         # self.pc = 0
         # self.acc = 0
-        self.id = Processo.proximo_id
+        self.pid = Processo.proximo_pid
         self.logica = logica
         self.tempo_chegada = tempo_chegada
         self.prioridade = prioridade
         self.quantum = quantum
         self.tempo_execucao = tempo_execucao
         self.tempo_restante = tempo_execucao
-        Processo.proximo_id += 1
+        Processo.proximo_pid += 1
         # Incluir "estado"?
 
     def __repr__(self):
-        return f"Processo {self.id} (prioridade: {self.prioridade}, quantum: {self.quantum}, tempo execução: {self.tempo_execucao}, tempo restante: {self.tempo_restante})"
+        return f"Processo {self.pid} (prioridade: {self.prioridade}, quantum: {self.quantum}, tempo execução: {self.tempo_execucao}, tempo restante: {self.tempo_restante})"
 
     # Carrega as instruções que estão no arquivo.txt direto na logica do processo
     def carregar_instrucoes(self, nome_do_arquivo):
